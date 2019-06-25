4db30772c182ce1bfd7da2d4c94148296d603fb2
xiaolin <462441355@qq.com>
2019-06-25 15:45:12 +0800

    * 增加token 判断

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,10 @@
 export const ERR_OK = 0
 
 // token非法
-export const ERROR_TOKEN_ILLEGAL = 1000
+export const ERROR_TOKEN_ILLEGAL = 1207
 
 // token过期
-export const ERROR_TOKEN_EXPIRE = 1001
+export const ERROR_TOKEN_EXPIRE = 1207
 
 // 权限不足
 export const ERROR_AUTHORITY = 1002
