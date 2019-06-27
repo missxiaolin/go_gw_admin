9d7822d79a493aac5506127ed36462c1ddc4c111
李铭昕 <715557344@qq.com>
2019-06-27 13:01:41 +0800

    修改错误码

--- src/api/config.js
+++ src/api/config.js
@@ -4,7 +4,7 @@ export const ERR_OK = 0
 export const ERROR_TOKEN_ILLEGAL = 501
 
 // token过期
-export const ERROR_TOKEN_EXPIRE = 1207
+export const ERROR_TOKEN_EXPIRE = 502
 
 // 权限不足
 export const ERROR_AUTHORITY = 1002
