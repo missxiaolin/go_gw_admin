c63cc080da7be7d69fe05e83743a0c1dc35387d1
xiaolin <462441355@qq.com>
2019-07-31 21:09:21 +0800

    * 分类列表增删改查

--- src/api/category.js
+++ src/api/category.js
@@ -1,12 +1,48 @@
 import fetch from '@/common/js/fetch'
 
 /**
- * 登录
+ * 获取分类列表
  * @param {*} query
  */
 export function getCategoryList (query) {
   return fetch({
     url: '/admin/category/list',
+    method: 'get',
+    params: query
+  })
+}
+
+/**
+ * 分类添加
+ * @param {*} query 
+ */
+export function createCategory (query) {
+  return fetch({
+    url: '/admin/category/add',
+    method: 'post',
+    data: query
+  })
+}
+
+/**
+ * 分类添加
+ * @param {*} query 
+ */
+export function updateCategory (query) {
+  return fetch({
+    url: '/admin/category/update',
+    method: 'post',
+    data: query
+  })
+}
+
+/**
+ * 分类添加
+ * @param {*} query 
+ */
+export function getCategoryInfo (query) {
+  return fetch({
+    url: '/admin/category/info',
     method: 'get',
     params: query
   })
--- src/view/category/list.vue
+++ src/view/category/list.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="app-container">
+        <div :className="'sub-navbar'" style="margin-bottom:30px;">
+            <template>
+                <el-button style="margin-left: 10px;" type="primary" @click="createCatrgory(0)">添加分类</el-button>
+            </template>
+        </div>
         <el-table
             :data="list"
             style="width: 100%"
@@ -14,11 +19,14 @@
             </el-table-column>
             <el-table-column
             prop="name"
-            label="分类名称">
+            label="分类名称"
+            >
             </el-table-column>
-            <el-table-column
-            prop="status"
-            label="是否显示">
+            <el-table-column label="状态" align="center">
+                <template slot-scope="scope">
+                    <span v-if="scope.row.status == 1">启用</span>
+                    <span v-else>禁用</span>
+                </template>
             </el-table-column>
             <el-table-column
             prop="created_at"
@@ -28,12 +36,42 @@
             prop="updated_at"
             label="修改时间">
             </el-table-column>
+            <el-table-column label="操作" align="center">
+                <template slot-scope="scope">
+                    <el-button size="mini" @click="createCatrgory(scope.row.id)">编辑</el-button>
+                </template>
+            </el-table-column>
         </el-table>
+        <el-dialog title="添加分类" :visible.sync="dialogTableVisible" :modal-append-to-body="false">
+            <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="120px">
+                <el-form-item label="分类名称" prop="name">
+                    <el-input name="name" type="text" v-model="ruleForm.name" placeholder="登录名"></el-input>
+                </el-form-item>
+                <el-form-item label="上级分类" prop="pid">
+                    <el-select v-model="ruleForm.pid" placeholder="请选择">
+                        <el-option
+                        v-for="item in formList"
+                        :key="item.id"
+                        :label="item.name"
+                        :value="item.id">
+                        </el-option>
+                    </el-select>
+                </el-form-item> 
+                <el-form-item label="状态" prop="status">
+                    <el-radio v-model="ruleForm.status" :label="1">启用</el-radio>
+                    <el-radio v-model="ruleForm.status" :label="0">禁用</el-radio>
+                </el-form-item> 
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit('ruleForm')">保存</el-button>
+                    <el-button  @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
     </div>
 </template>
 
 <script>
-import { getCategoryList } from "api/category"
+import { getCategoryList, createCategory, updateCategory, getCategoryInfo } from "api/category"
 import { ERR_OK } from '@/api/config'
 import { Message } from 'element-ui'
 
@@ -43,24 +81,115 @@ export default {
         return {
             list: [],
             searchForm: {
-                pid: 0,
-                name: ""
+                pid: null,
+                name: "",
+                status: 1
             },
+            dialogTableVisible: false,
+            rules: {
+                name: [{
+                    required: true,
+                    message: '分类名称',
+                    trigger: 'blur'
+                }],
+            },
+            formList: [],
+            ruleForm: {
+                id: 0,
+                name: "",
+                pid: 0,
+                status: 1
+            }
         }
     },
     mounted () {
-        
-        this.lists(this.searchForm, res => {
-            let data = []
-            res.forEach(item => {
-                
-                item.hasChildren = true
-                data.push(item)
-            })
-            this.list = data
-        })
+        this.getList()
     },
     methods: {
+        createCatrgory (id) {
+            this.dialogTableVisible = true
+            this.ruleForm.id = 0
+            this.ruleForm.name = ""
+            this.ruleForm.pid = 0
+            this.ruleForm.status = 1
+            if( id ) {
+                this.getInfo(id)
+            }
+        },
+        // 验证
+        onSubmit (formName) {
+            let self = this
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    if (this.ruleForm.id) {
+                        self.update()
+                    } else {
+                        self.add()
+                    }
+                
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+        },
+        // 创建
+        async add () {
+            let res = await createCategory(this.ruleForm)
+            this.dialogTableVisible = false
+            this.searchForm.pid = 0
+            if (res.success != ERR_OK) {
+                Message(res.msg)
+                return
+            }
+            this.getList()
+        },
+        // 修改
+        async update () {
+            let res = await updateCategory(this.ruleForm)
+            this.dialogTableVisible = false
+            this.searchForm.pid = 0
+            if (res.success != ERR_OK) {
+                Message(res.msg)
+                return
+            }
+            this.getList()
+        },
+        // 获取详情
+        async getInfo (id) {
+            let param = {
+                id: id
+            }
+            let res = await getCategoryInfo(param)
+            if (res.success == ERR_OK) {
+                this.ruleForm.id = res.data.id
+                this.ruleForm.name = res.data.name
+                this.ruleForm.pid = res.data.pid
+                this.ruleForm.status = res.data.status
+            } else {
+                Message(res.msg)
+            }
+        },
+        getList () {
+            this.formList = []
+            this.list = []
+            this.lists(this.searchForm, res => {
+                let data = [],
+                    formList = []
+                    formList.push({
+                        id: 0,
+                        name: "请选择",
+                    })
+                res.forEach(item => {
+                    item.hasChildren = true
+                    data.push(item)
+                    formList.push(item)
+                })
+                this.formList = formList
+                this.list = data
+            })
+        },
+        // 获取列表
         lists (param, cal) {
             getCategoryList(this.searchForm).then(res => {
                 if (res.success == ERR_OK) {
@@ -71,6 +200,7 @@ export default {
             })
 
         },
+        // 二级列表
         load(tree, treeNode, resolve) {
             this.searchForm.pid = tree.id
             this.lists(this.searchForm, res => {
